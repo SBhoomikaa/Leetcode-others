1a5c26e9f99ccfc0d29e81f2ddacc8fcf1d1a6eb
SBhoomikaa <159709013+SBhoomikaa@users.noreply.github.com>
2024-12-05 00:12:26 +0530

    Time: 5 ms (12.42%) | Memory: 15.3 MB (49.80%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,19 @@
+int maxProfit(int* prices, int pricesSize) {
+    int sell=0,buy=prices[0],i,temp=prices[0],k=0;
+    for(i=1;i<pricesSize;i++){
+        if(prices[i]<temp){
+        temp=prices[i];
+        k=0;
+        continue;
+        }
+        if(prices[i]>temp){
+       k=prices[i];}
+        if(k-temp>sell-buy){
+            buy=temp;
+            sell=k;
+        }
+    }
+    if(sell>buy)
+    return sell-buy;
+    return 0;
+}
