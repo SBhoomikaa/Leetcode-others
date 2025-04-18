a118f066278d852c413772295f73927ac9c0c345
Bhoomika Sundar <159709013+SBhoomikaa@users.noreply.github.com>
2025-04-18 20:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (3.20%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.c
@@ -0,0 +1,22 @@
+int tribonacci(int n) {
+    int *dp=(int*)malloc(sizeof(int)*(n+1));
+    if(n>2){
+    dp[0]=0;
+    dp[1]=1;
+    dp[2]=1;
+    int count=3;
+    while(count<=n){
+        dp[count]=dp[count-1]+dp[count-2]+dp[count-3];
+        count++;
+    }
+    return dp[n];
+    }
+    else{
+        switch(n){
+            case 0:return 0;
+            case 1:return 1;
+            case 2:return 1;
+        }
+    }
+    return 0;
+}
