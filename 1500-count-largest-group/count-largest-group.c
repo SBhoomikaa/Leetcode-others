5e76044082bff32e6e69fbe4dd995e5abe1c3bec
Bhoomika Sundar <159709013+SBhoomikaa@users.noreply.github.com>
2025-04-23 10:34:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (71.43%) - LeetSync

--- 1500-count-largest-group/count-largest-group.c
+++ 1500-count-largest-group/count-largest-group.c
@@ -10,14 +10,14 @@ int comp(const void *a, const void *b) {
     return (*(int *)b - *(int *)a);
 }
 int countLargestGroup(int n) {
-    int *p=(int*)calloc(n+1,sizeof(int));
+    int *p=(int*)calloc(37,sizeof(int));
     int s,j=0;
     for(int i=1;i<=n;i++){
         s=sum(i);
         (p[s])+=1;
     }
-    qsort(p, n+1, sizeof(int),comp);
-    while(j<=n){
+    qsort(p, 37, sizeof(int),comp);
+    while(j<=36){
         if(p[j]!=p[j+1])
             break;   
         j++;
