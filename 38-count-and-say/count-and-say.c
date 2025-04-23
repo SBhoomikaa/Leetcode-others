efc241f9c410f05ca793547f5712a7d3820b08cc
Bhoomika Sundar <159709013+SBhoomikaa@users.noreply.github.com>
2025-04-23 21:51:47 +0530

    Time: 2 ms (53.06%) | Memory: 8.3 MB (91.20%) - LeetSync

---
+++ 38-count-and-say/count-and-say.c
@@ -0,0 +1,27 @@
+char* rle(char *p){
+    int i=0,count,k=0,y;
+    char* s=(char*)malloc((sizeof(char)*strlen(p)*2)+1);
+    char x;
+    y=strlen(p);
+    while(i<y){
+        count=1;
+        x=p[i];
+        while(p[i]==p[i+1] && i<y-1){
+            count++;
+            i++;
+        }    
+        i++;
+        s[k++]=(char)(count+'0');
+        s[k++]=x;   
+    }
+    s[k]='\0';
+    return s;
+}
+char* countAndSay(int n) {
+    int i,count,j;
+    if(n==1)
+         return "1"; 
+    char *o;    
+    o=rle(countAndSay(n-1));
+    return o;   
+}
