be8f5815641a23ad8dc497282c6ccf6c72f01dd3
Bhoomika Sundar <159709013+SBhoomikaa@users.noreply.github.com>
2025-07-22 23:42:55 +0530

    Time: 30 ms (11.90%) | Memory: 9.8 MB (64.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
@@ -2,8 +2,8 @@ int lengthOfLongestSubstring(char* s) {
     if(strlen(s)==0|| strlen(s)==1)
         return strlen(s);
     int map[256]={0};    
-    int left=0,right=1,k=1,K=k;
-    map[s[0]]=1;
+    int left=0,right=0,k=0,K=k;
+    //map[s[0]]=1;
     while(left<=right && right<strlen(s)){
         if(map[s[right]]==0){
             map[s[right]]+=1;
