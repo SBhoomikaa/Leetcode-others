1575cc557d537ee7cb1248a5be0183f4b65a9521
Bhoomika Sundar <159709013+SBhoomikaa@users.noreply.github.com>
2025-07-23 23:50:57 +0530

    Time: 7 ms (80.90%) | Memory: 19.4 MB (22.05%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,12 +2,10 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s=s.lower()
         clean= re.sub(r'[^a-z0-9]', '', s)
-        i=0
-        j=len(clean)-1
+        i,j=0,len(clean)-1
         while(i<j):
-            if(clean[i]==clean[j]):
-                i+=1
-                j-=1
-            else:
-                return False;    
+            if(clean[i]!=clean[j]):
+                return False
+            i+=1
+            j-=1
         return True
