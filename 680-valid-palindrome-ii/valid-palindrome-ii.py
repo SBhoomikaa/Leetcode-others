a3acb36fcc413610cc4d2c1b2e24067b2faf7c2b
Bhoomika Sundar <159709013+SBhoomikaa@users.noreply.github.com>
2025-07-24 00:30:13 +0530

    Time: 60 ms (64.36%) | Memory: 17.8 MB (73.71%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rec(self,s,start,end,k):
+        i,j=start,end
+        x=k
+        while(i<j):
+            if(s[i]!=s[j]):
+                if(x==1):
+                    return x+1
+                x+=1
+                a=self.rec(s,i+1,j,x)
+                b=self.rec(s,i,j-1,x)
+                return min(a,b)
+            i+=1
+            j-=1
+        return x            
+
+
+    def validPalindrome(self, s: str) -> bool:
+        k=self.rec(s,0,len(s)-1,0)
+        if(k==0 or k==1):
+            return True
+        else:
+            return False    
